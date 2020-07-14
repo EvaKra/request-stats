4bed39e3364639c97080de08304d943a09d85e2a
EvaKra <eva.kramer91@gmail.com>
2020-07-14 16:11:37 +0100

    message for invalid requests with invalid customer_id

--- interface.rb
+++ interface.rb
@@ -6,7 +6,11 @@ customer_id = gets.chomp
 puts "Which date (DD/MM/YYYY)?"
 date = gets.chomp
 
-p "Requests from #{Customer.find(customer_id).name}(Id: #{customer_id}) on #{date}: #{requests_customer_per_day(customer_id, date)}"
+if Customer.exists?(customer_id)
+    p "Requests from #{Customer.find(customer_id).name} (Customer_id: #{customer_id}) on #{date}: #{requests_customer_per_day(customer_id, date)}"
+else
+    p "Requests from non existing customers on #{date}: #{requests_customer_per_day(customer_id, date)}"
+end
 
 p "Total number of requests on #{date}: #{all_requests_per_day(date)}"
 p "Valid requests on #{date}: #{request_total_per_day(date)}"
